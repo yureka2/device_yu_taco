0c9cb5de362185cf265cff440392345dc7777826
Nikolay Karev <karev.n@gmail.com>
2017-11-03 14:35:24 +0500

    Updated LED HAL

--- liblight/lights.c
+++ liblight/lights.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
  * Copyright (C) 2014 The  Linux Foundation. All rights reserved.
+ * Copyright (C) 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,8 +28,8 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <math.h>
 
-#include <sys/ioctl.h>
 #include <sys/types.h>
 
 #include <hardware/lights.h>
@@ -39,12 +40,19 @@ static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 static struct light_state_t g_notification;
 static struct light_state_t g_battery;
-static int g_attention = 0;
-
-#define RAMP_STEP_DURATION 1
+static struct light_state_t g_attention;
 
 char const*const RED_LED_FILE
         = "/sys/class/leds/red/brightness";
+
+char const*const RED_BLINK_FILE
+	= "/sys/class/leds/red/blink";
+
+char const*const GREEN_BLINK_FILE
+	= "/sys/class/leds/green/blink";
+
+char const*const BLUE_BLINK_FILE
+	= "/sys/class/leds/blue/blink";
 
 char const*const GREEN_LED_FILE
         = "/sys/class/leds/green/brightness";
@@ -55,26 +63,106 @@ char const*const BLUE_LED_FILE
 char const*const LCD_FILE
         = "/sys/class/leds/lcd-backlight/brightness";
 
+char const*const RED_BREATH_FILE
+        = "/sys/class/leds/red/led_time";
+
+char const*const GREEN_BREATH_FILE
+        = "/sys/class/leds/green/led_time";
+
+char const*const BLUE_BREATH_FILE
+        = "/sys/class/leds/blue/led_time";
+
 char const*const BUTTON_FILE
         = "/sys/class/leds/button-backlight/brightness";
 
-char const*const RED_BLINK_FILE
-        = "/sys/class/leds/red/blink";
+struct color {
+    unsigned int r, g, b;
+    float _L, _a, _b;
+};
 
-char const*const GREEN_BLINK_FILE
-        = "/sys/class/leds/green/blink";
+// this hardware only allows primary colors
+static struct color colors[] = {
+    { 255,   0,   0, 0, 0, 0 }, // red
+    { 255, 255,   0, 0, 0, 0 }, // yellow
+    {   0, 255,   0, 0, 0, 0 }, // green
+    {   0, 255, 255, 0, 0, 0 }, // cyan
+    {   0,   0, 255, 0, 0, 0 }, // blue
+    { 255,   0, 255, 0, 0, 0 }, // magenta
+    { 255, 255, 255, 0, 0, 0 }, // white
+    { 127, 127, 127, 0, 0, 0 }, // grey
+    {   0,   0,   0, 0, 0, 0 }, // black
+};
 
-char const*const BLUE_BLINK_FILE
-        = "/sys/class/leds/blue/blink";
+#define MAX_COLOR 9
 
-char const*const RED_BLINK_TIMING_FILE
-        = "/sys/class/leds/red/led_time";
+// Convert RGB to L*a*b colorspace
+// from http://www.brucelindbloom.com
+static void rgb2lab(unsigned int R, unsigned int G, unsigned int B,
+                    float *_L, float *_a, float *_b) {
 
-char const*const GREEN_BLINK_TIMING_FILE
-        = "/sys/class/leds/green/led_time";
+    float r, g, b, X, Y, Z, fx, fy, fz, xr, yr, zr;
+    float Ls, as, bs;
+    float eps = 216.f / 24389.f;
+    float k = 24389.f / 27.f;
 
-char const*const BLUE_BLINK_TIMING_FILE
-        = "/sys/class/leds/blue/led_time";
+    float Xr = 0.964221f;  // reference white D50
+    float Yr = 1.0f;
+    float Zr = 0.825211f;
+
+    // RGB to XYZ
+    r = R / 255.f; //R 0..1
+    g = G / 255.f; //G 0..1
+    b = B / 255.f; //B 0..1
+
+    // assuming sRGB (D65)
+    if (r <= 0.04045)
+        r = r / 12;
+    else
+        r = (float) pow((r + 0.055) / 1.055, 2.4);
+
+    if (g <= 0.04045)
+        g = g / 12;
+    else
+        g = (float) pow((g + 0.055) / 1.055, 2.4);
+
+    if (b <= 0.04045)
+        b = b / 12;
+    else
+        b = (float) pow((b + 0.055) / 1.055, 2.4);
+
+
+    X = 0.436052025f * r + 0.385081593f * g + 0.143087414f * b;
+    Y = 0.222491598f * r + 0.71688606f * g + 0.060621486f * b;
+    Z = 0.013929122f * r + 0.097097002f * g + 0.71418547f * b;
+
+    // XYZ to Lab
+    xr = X / Xr;
+    yr = Y / Yr;
+    zr = Z / Zr;
+
+    if (xr > eps)
+        fx = (float) pow(xr, 1 / 3.);
+    else
+        fx = (float) ((k * xr + 16.) / 116.);
+
+    if (yr > eps)
+        fy = (float) pow(yr, 1 / 3.);
+    else
+        fy = (float) ((k * yr + 16.) / 116.);
+
+    if (zr > eps)
+        fz = (float) pow(zr, 1 / 3.);
+    else
+        fz = (float) ((k * zr + 16.) / 116);
+
+    Ls = (116 * fy) - 16;
+    as = 500 * (fx - fy);
+    bs = 200 * (fy - fz);
+
+    *_L = (2.55 * Ls + .5);
+    *_a = (as + .5);
+    *_b = (bs + .5);
+}
 
 /**
  * device methods
@@ -82,11 +170,19 @@ char const*const BLUE_BLINK_TIMING_FILE
 
 void init_globals(void)
 {
+    int i = 0;
+    for (i = 0; i < MAX_COLOR; i++) {
+        rgb2lab(colors[i].r, colors[i].g, colors[i].b,
+                &colors[i]._L, &colors[i]._a, &colors[i]._b);
+    }
+
     // init the mutex
     pthread_mutex_init(&g_lock, NULL);
+
 }
 
-static int write_int(char const* path, int value)
+static int
+write_int(char const* path, int value)
 {
     int fd;
     static int already_warned = 0;
@@ -107,29 +203,29 @@ static int write_int(char const* path, int value)
     }
 }
 
-static int write_str(char const* path, char* value)
+static int
+write_str(char const* path, char *value)
 {
     int fd;
     static int already_warned = 0;
 
     fd = open(path, O_RDWR);
     if (fd >= 0) {
-        char buffer[1024];
-        int bytes = snprintf(buffer, sizeof(buffer), "%s\n", value);
-        ssize_t amt = write(fd, buffer, (size_t)bytes);
+        char buffer[20];
+        ssize_t amt = write(fd, value, (size_t)strlen(value));
         close(fd);
         return amt == -1 ? -errno : 0;
     } else {
         if (already_warned == 0) {
-            ALOGE("%s: failed to open %s\n", __func__, path);
+            ALOGE("write_str failed to open %s\n", path);
             already_warned = 1;
         }
         return -errno;
     }
 }
 
-
-static int is_lit(struct light_state_t const* state)
+static int
+is_lit(struct light_state_t const* state)
 {
     return state->color & 0x00ffffff;
 }
@@ -140,6 +236,37 @@ rgb_to_brightness(struct light_state_t const* state)
     int color = state->color & 0x00ffffff;
     return ((77*((color>>16)&0x00ff))
             + (150*((color>>8)&0x00ff)) + (29*(color&0x00ff))) >> 8;
+}
+
+// find the color with the shortest distance
+static struct color *
+nearest_color(unsigned int r, unsigned int g, unsigned int b)
+{
+    int i = 0;
+    float _L, _a, _b;
+    double L_dist, a_dist, b_dist, total;
+    double distance = 3 * 255;
+
+    struct color *nearest = NULL;
+
+    rgb2lab(r, g, b, &_L, &_a, &_b);
+
+    ALOGV("%s: r=%d g=%d b=%d L=%f a=%f b=%f", __func__,
+            r, g, b, _L, _a, _b);
+
+    for (i = 0; i < MAX_COLOR; i++) {
+        L_dist = pow(_L - colors[i]._L, 2);
+        a_dist = pow(_a - colors[i]._a, 2);
+        b_dist = pow(_b - colors[i]._b, 2);
+        total = sqrt(L_dist + a_dist + b_dist);
+        ALOGV("%s: total %f distance %f", __func__, total, distance);
+        if (total < distance) {
+            nearest = &colors[i];
+            distance = total;
+        }
+    }
+
+    return nearest;
 }
 
 static int
@@ -157,129 +284,27 @@ set_light_backlight(struct light_device_t* dev,
     return err;
 }
 
-static inline int set_notification_led(unsigned int color) {
-  int red, green, blue;
-  red = (color >> 16) & 0xFF;
-  green = (color >> 8) & 0xFF;
-  blue = color & 0xFF;
-  write_int(RED_LED_FILE, red);
-  write_int(GREEN_LED_FILE, green);
-  write_int(BLUE_LED_FILE, blue);
-
-  // Disable blinking
-  if (red == 0 && green == 0 && blue == 0) {
-      write_int(RED_BLINK_FILE, 0);
-      write_int(GREEN_BLINK_FILE, 0);
-      write_int(BLUE_BLINK_FILE, 0);
-  }
-  return 0;
-}
-
-
-static inline int set_notification_blink(unsigned int color, int on, int off) {
-  int red, green, blue;
-  int rise, hold, fall;
-
-  set_notification_led(0);
-
-  red = (color >> 16) & 0xFF;
-  green = (color >> 8) & 0xFF;
-  blue = color & 0xFF;
-
-  //red = red * 0.21;
-  //green = green * 0.71;
-  //blue = blue * 0.07;
-
-  rise = RAMP_STEP_DURATION;
-  hold = on - (rise * 2);
-  fall = RAMP_STEP_DURATION;
-  if (rise + fall > hold) {
-    hold = 0;
-    rise = hold / 2;
-    fall = hold / 2;
-  }
-
-  char buffer[20];
-  memset(buffer, 0, 20 * sizeof(char));
-  snprintf(buffer, sizeof(buffer), "%d %d %d %d\n", rise, hold, fall, off);
-
-  if (red > green + blue) {
-      write_int(RED_BLINK_FILE, 1);
-      write_str(RED_BLINK_TIMING_FILE, buffer);
-      write_int(GREEN_BLINK_FILE, 0);
-      write_int(BLUE_BLINK_FILE, 0);
-      ALOGW("Blink red");
-  }
-
-  if (green > red + blue) {
-      write_int(GREEN_BLINK_FILE, 1);
-      write_str(GREEN_BLINK_TIMING_FILE, buffer);
-      write_int(RED_BLINK_FILE, 0);
-      write_int(BLUE_BLINK_FILE, 0);
-      ALOGW("Blink green");
-  }
-
-  if (blue > green + red) {
-    write_int(BLUE_BLINK_FILE, 1);
-    write_str(BLUE_BLINK_TIMING_FILE, buffer);
-    write_int(GREEN_BLINK_FILE, 0);
-    write_int(RED_BLINK_FILE, 0);
-    ALOGW("Blink white");
-  }
-
-  if (red == green && red == blue) {
-    write_int(RED_BLINK_FILE, 1);
-    write_str(RED_BLINK_TIMING_FILE, buffer);
-    write_int(GREEN_BLINK_FILE, 1);
-    write_str(GREEN_BLINK_TIMING_FILE, buffer);
-    write_int(BLUE_BLINK_FILE, 1);
-    write_str(BLUE_BLINK_TIMING_FILE, buffer);
-    ALOGW("Blink white");
-  }
-
-  if (red < (green + blue) / 2) {
-    write_int(BLUE_BLINK_FILE, 1);
-    write_str(BLUE_BLINK_TIMING_FILE, buffer);
-    write_int(GREEN_BLINK_FILE, 1);
-    write_str(GREEN_BLINK_TIMING_FILE, buffer);
-    write_int(RED_BLINK_FILE, 0);
-    ALOGW("Blink cyan");
-  }
-  if (green < (red + blue) / 2) {
-    write_int(BLUE_BLINK_FILE, 1);
-    write_str(BLUE_BLINK_TIMING_FILE, buffer);
-    write_int(RED_BLINK_FILE, 1);
-    write_str(RED_BLINK_TIMING_FILE, buffer);
-    write_int(GREEN_BLINK_FILE, 0);
-    ALOGW("Blink magenta");
-  }
-
-  if (blue < (red + green) / 2) {
-    write_int(RED_BLINK_FILE, 1);
-    write_str(RED_BLINK_TIMING_FILE, buffer);
-    write_int(GREEN_BLINK_FILE, 1);
-    write_str(GREEN_BLINK_TIMING_FILE, buffer);
-    write_int(BLUE_BLINK_FILE, 0);
-    ALOGW("Blink orange");
-  }
-  return 0;
-}
-
-
-static inline int disable_notification_led() {
-  return set_notification_led(0);
-}
-
-static int set_speaker_light_locked(struct light_device_t* dev,
+static int
+set_speaker_light_locked(struct light_device_t* dev,
         struct light_state_t const* state)
 {
     int red, green, blue;
     int blink;
     int onMS, offMS;
     unsigned int colorRGB;
+    char breath_pattern[64] = { 0, };
+    struct color *nearest = NULL;
 
     if(!dev) {
         return -1;
+    }
+
+    write_int(RED_LED_FILE, 0);
+    write_int(GREEN_LED_FILE, 0);
+    write_int(BLUE_LED_FILE, 0);
+
+    if (state == NULL) {
+        return 0;
     }
 
     switch (state->flashMode) {
@@ -296,29 +321,73 @@ static int set_speaker_light_locked(struct light_device_t* dev,
 
     colorRGB = state->color;
 
-
-    ALOGW("set_speaker_light_locked mode %d, colorRGB=%08X, onMS=%d, offMS=%d\n",
+    ALOGD("set_speaker_light_locked mode %d, colorRGB=%08X, onMS=%d, offMS=%d\n",
             state->flashMode, colorRGB, onMS, offMS);
-    if (onMS > 0 && offMS > 0) {
-        set_notification_blink(colorRGB, onMS, offMS);
+
+    red = (colorRGB >> 16) & 0xFF;
+    green = (colorRGB >> 8) & 0xFF;
+    blue = colorRGB & 0xFF;
+
+    blink = onMS > 0 && offMS > 0;
+
+    if (blink) {
+        // Driver doesn't permit us to set individual duty cycles, so only
+        // pick pure colors at max brightness when blinking.
+        nearest = nearest_color(red, green, blue);
+
+        red = nearest->r;
+        green = nearest->g;
+        blue = nearest->b;
+
+        // Make sure the values are between 1 and 7 seconds
+        if (onMS < 1000)
+            onMS = 1000;
+        else if (onMS > 7000)
+            onMS = 7000;
+
+        if (offMS < 1000)
+            offMS = 1000;
+        else if (offMS > 7000)
+            offMS = 7000;
+
+        // ramp up, lit, ramp down, unlit. in seconds.
+        sprintf(breath_pattern,"1 %d 1 %d",(int)(onMS/1000),(int)(offMS/1000));
+
     } else {
-        set_notification_led(colorRGB);
+        blink = 0;
+        sprintf(breath_pattern,"1 2 1 2");
     }
+
+    // Do everything with the lights out, then turn up the brightness
+    write_str(RED_BREATH_FILE, breath_pattern);
+    write_int(RED_BLINK_FILE, (blink && red ? 1 : 0));
+    write_str(GREEN_BREATH_FILE, breath_pattern);
+    write_int(GREEN_BLINK_FILE, (blink && green ? 1 : 0));
+    write_str(BLUE_BREATH_FILE, breath_pattern);
+    write_int(BLUE_BLINK_FILE, (blink && blue ? 1 : 0));
+
+    write_int(RED_LED_FILE, red);
+    write_int(GREEN_LED_FILE, green);
+    write_int(BLUE_LED_FILE, blue);
 
     return 0;
 }
 
-static void handle_speaker_battery_locked(struct light_device_t* dev)
+static void
+handle_speaker_battery_locked(struct light_device_t* dev)
 {
-    if (is_lit(&g_attention))
+    set_speaker_light_locked(dev, NULL);
+    if (is_lit(&g_attention)) {
         set_speaker_light_locked(dev, &g_attention);
-    else if (is_lit(&g_notification))
+    } else if (is_lit(&g_notification)) {
         set_speaker_light_locked(dev, &g_notification);
-    else
+    } else {
         set_speaker_light_locked(dev, &g_battery);
+    }
 }
 
-static int set_light_battery(struct light_device_t* dev,
+static int
+set_light_battery(struct light_device_t* dev,
         struct light_state_t const* state)
 {
     pthread_mutex_lock(&g_lock);
@@ -328,7 +397,8 @@ static int set_light_battery(struct light_device_t* dev,
     return 0;
 }
 
-static int set_light_notifications(struct light_device_t* dev,
+static int
+set_light_notifications(struct light_device_t* dev,
         struct light_state_t const* state)
 {
     pthread_mutex_lock(&g_lock);
@@ -338,17 +408,24 @@ static int set_light_notifications(struct light_device_t* dev,
     return 0;
 }
 
-static int set_light_attention(struct light_device_t* dev,
+static int
+set_light_attention(struct light_device_t* dev,
         struct light_state_t const* state)
 {
     pthread_mutex_lock(&g_lock);
+
+    g_attention = *state;
     if (state->flashMode == LIGHT_FLASH_HARDWARE) {
-        g_attention = state->flashOnMS;
+        if (g_attention.flashOnMS > 0 && g_attention.flashOffMS == 0) {
+            g_attention.flashMode = LIGHT_FLASH_NONE;
+        }
     } else if (state->flashMode == LIGHT_FLASH_NONE) {
-        g_attention = 0;
+        g_attention.color = 0;
     }
     handle_speaker_battery_locked(dev);
+
     pthread_mutex_unlock(&g_lock);
+
     return 0;
 }
 
@@ -396,10 +473,10 @@ static int open_lights(const struct hw_module_t* module, char const* name,
         set_light = set_light_battery;
     else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
         set_light = set_light_notifications;
-    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
-        set_light = set_light_buttons;
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
         set_light = set_light_attention;
+    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+        set_light = set_light_buttons;
     else
         return -EINVAL;
 
@@ -434,7 +511,7 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .version_major = 1,
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "lights Module",
-    .author = "Google, Inc.",
+    .name = "Lights Module",
+    .author = "The CyanogenMod Project",
     .methods = &lights_module_methods,
 };
