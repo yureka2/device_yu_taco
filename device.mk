d8517c78d75fe3f9ea38de9e01af2b781897fca4
Nikolay Karev <karev.n@gmail.com>
2018-01-13 16:01:25 +0500

    perf profiles

--- device.mk
+++ device.mk
@@ -242,6 +242,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/etc/perf-profile0.conf:system/vendor/etc/perf-profile0.conf \
     $(LOCAL_PATH)/configs/etc/perf-profile1.conf:system/vendor/etc/perf-profile1.conf \
+		$(LOCAL_PATH)/configs/etc/perf-profile2.conf:system/vendor/etc/perf-profile2.conf \
+		$(LOCAL_PATH)/configs/etc/perf-profile3.conf:system/vendor/etc/perf-profile3.conf \
+		$(LOCAL_PATH)/configs/etc/perf-profile4.conf:system/vendor/etc/perf-profile4.conf \
+		$(LOCAL_PATH)/configs/etc/perf-profile5.conf:system/vendor/etc/perf-profile5.conf \
 
 # Power
 PRODUCT_PACKAGES += \
